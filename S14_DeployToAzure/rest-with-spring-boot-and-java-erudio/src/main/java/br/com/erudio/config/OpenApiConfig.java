cbf04c152d6c84758f12f4aba7f8498eaf7f512f
leandrocgsi <leandrocgsi@gmail.com>
2023-07-01 19:20:37 -0300

    Dockerizing our Application - After

--- S14_DeployToAzure/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/OpenApiConfig.java
+++ S14_DeployToAzure/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -14,7 +14,7 @@ public class OpenApiConfig {
 	OpenAPI customOpenAPI() {
 		return new OpenAPI()
 			.info(new Info()
-				.title("RESTful API with Java 19 and Spring Boot 3.0.1")
+				.title("Hello Microsoft Azure")
 				.version("v1")
 				.description("Some description about your API")
 				.termsOfService("https://pub.erudio.com.br/meus-cursos")
